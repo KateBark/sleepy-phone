dcdbaefed26e6906db05653b6de9d8bba72e25f9
Kate Bark <barkkate@gmail.com>
2026-01-10 18:29:04 -0500

    added puppy images to public folder, updated sleeping emoji to sleeping puppy, background not transparent - need to fix

--- components/PetDisplay.js
+++ components/PetDisplay.js
@@ -1,12 +1,31 @@
 import { PET_STATES } from "@/lib/constants";
+import Image from "next/image";
 
 export default function PetDisplay({ state }) {
-  const emoji =
-    state === PET_STATES.SLEEPING
-      ? "😴"
-      : state === PET_STATES.HAPPY
-      ? "😊"
-      : "🐣";
+  const display =
+    state === PET_STATES.SLEEPING ? (
+      <Image
+        src="/sleeping-puppy.png"
+        alt="Sleeping Puppy"
+        width={64}
+        height={64}
+      />
+    ) : state === PET_STATES.HAPPY ? (
+      "😊"
+    ) : (
+      "🐣"
+    );
 
-  return <div style={{ fontSize: "4rem" }}>{emoji}</div>;
+  return (
+    <div
+      style={{
+        fontSize: "4rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {display}
+    </div>
+  );
 }
